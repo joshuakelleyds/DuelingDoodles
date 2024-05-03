f7bcbecd9ee43492db5175be34b39a8f878a74df
Josh Kelley <Joshua Kelley>
2024-05-03 00:59:23 -0400

    fixed and updated

--- src/worker.js
+++ src/worker.js
@@ -1,5 +1,4 @@
 import constants from "./constants";
-
 import { pipeline, env, RawImage } from "@xenova/transformers";
 
 // Disable local models
@@ -26,7 +25,6 @@ class Singleton {
                 progress_callback,
             });
         }
-
         return this.instance;
     }
 }
@@ -35,22 +33,26 @@ self.addEventListener("message", async (event) => {
     const message = event.data;
 
     if (message.action === 'load') {
+        // Load the image classification model when the 'load' action is received
         await ImageClassificationPipelineSingleton.getInstance();
         self.postMessage({ status: "ready" });
         return;
     }
 
-    // Convert RGBA to grayscale, choose based on alpha channel
+    // Convert RGBA image to grayscale based on the alpha channel
     const data = new Uint8ClampedArray(message.image.data.length / 4);
     for (let i = 0; i < data.length; ++i) {
         data[i] = message.image.data[i * 4 + 3];
     }
+
+    // Create a RawImage object from the grayscale data
     const img = new RawImage(data, message.image.width, message.image.height, 1);
 
+    // Classify the image
     let result = await classify(img);
     if (result === null) return;
 
-    // Send the result back to the main thread
+    // Send the classification result back to the main thread
     self.postMessage({
         status: "result",
         task: "image-classification",
@@ -65,13 +67,14 @@ class ImageClassificationPipelineSingleton extends Singleton {
 }
 
 const classify = async (image) => {
-
+    // Get an instance of the image classification model
     let classifier = await ImageClassificationPipelineSingleton.getInstance();
 
-    // Actually run classification
+    // Run the image classification
     let output = await classifier(image, {
         topk: 0, // Return all classes
     }).catch((error) => {
+        // If an error occurs during classification, send an error message to the main thread
         self.postMessage({
             status: "error",
             task: "image-classification",
